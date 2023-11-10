afc7702e24f72ad35ad63d42cf6da7b0c2207774
john.rodriguez <john.rodriguez@gmail.com>
2023-11-09 21:36:49 -0500

    I have added formlogin just like example

--- oauth2/src/main/java/com/oauth2/config/WebSecurityConfig.java
+++ oauth2/src/main/java/com/oauth2/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.oauth2.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -31,5 +32,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationManager authenticacionManagerBean() throws Exception {
         return  super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http.formLogin();
     }
 }

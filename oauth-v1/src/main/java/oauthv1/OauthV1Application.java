0b399c60e4b3c03e3e4252b13a03f86dc5f5dac0
john.rodriguez <john.rodriguez@gmail.com>
2023-11-09 21:41:24 -0500

    first commit another example

---
+++ oauth-v1/src/main/java/oauthv1/OauthV1Application.java
@@ -0,0 +1,13 @@
+package oauthv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthV1Application.class, args);
+	}
+
+}
---
+++ oauth-v1/src/test/java/oauthv1/OauthV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package oauthv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
